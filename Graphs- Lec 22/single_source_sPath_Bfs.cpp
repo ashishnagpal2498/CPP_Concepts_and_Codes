87ed2f3dbfb0f20d77f44308b09e240300433cd6
ashish nagpal <ashishnagpal2498@gmail.com>
2019-03-08 20:36:11 +0530

    Graphs - Single source shortest path

--- Graphs- Lec 22/single_source_sPath_Bfs.cpp
+++ Graphs- Lec 22/single_source_sPath_Bfs.cpp
@@ -38,6 +38,73 @@ public:
     }
     // Breadth First Search 
     //Queue - 
+    void bfs(T s)
+    {
+        // visited - 
+        // Queue
+        queue<T> q1;
+        map<T,bool> visited;
+
+        q1.push(s);
+        visited[s] = true;
+
+        while(!q1.empty())
+        {
+            T node = q1.front();
+            q1.pop();
+            cout<<node<<"  ";
+
+            for(auto vert:adjList[node])
+            {
+                if(!visited[vert])
+                {
+                    q1.push(vert);
+                    visited[vert] = true;
+                }
+            }
+        }
+    }
+    // Single source shortest path -
+    int bfs_shortest_path(T s,T d)
+    {
+        // Queue - and Distance hash-map
+        queue<T> q1;
+        map<T,int> dist;
+        map<T,T> parent;
+        // Making all other values as INFINITY
+        for(auto i:adjList)
+        {
+            dist[i.first] = INT_MAX;
+        }
+        q1.push(s);
+        dist[s] = 0;
+
+        while(!q1.empty())
+        {
+            T node = q1.front();
+            q1.pop();
+            cout<<node<<"  ";
+
+            for(auto neigh:adjList[node])
+            {
+                if(dist[neigh]==INT_MAX)
+                {
+                    q1.push(neigh);
+                    dist[neigh] = dist[node] +1;
+                    parent[neigh] = node;
+                }
+            }
+        }
+        // Print the distance;
+        for(auto i:adjList)
+        {
+            T node = i.first;
+            cout<<"Distance of "<<node<<" From source node  "<<s<<" is  "<<dist[node]<<endl;
+        }
+
+            // return destination node distance
+    return dist[d];
+    }
 
 };
 int main()
@@ -48,5 +115,10 @@ int main()
     g.addEdge(3,4);
     g.addEdge(4,1);
     g.print();
+    cout<<endl;
+    g.bfs(1);
+    cout<<endl;
+    int ans = g.bfs_shortest_path(1,3);
+    cout<<endl<<"Distance is "<<ans;
     return 0;
 }
