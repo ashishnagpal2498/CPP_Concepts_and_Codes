59b0238bab5076fba95d60d3e418a6d0ed87e162
ashish nagpal <ashishnagpal2498@gmail.com>
2019-06-17 23:21:43 +0530

    Pointers and Arrays

--- Practice_June_July_2019/4) Pointers/basic_of_pointers.cpp
+++ Practice_June_July_2019/4) Pointers/basic_of_pointers.cpp
@@ -1,1 +1,41 @@
-//Pointers 
+//Pointers - and Pointers and Functions
+#include<iostream>
+using namespace std;
+void array_fun(int a[])
+{
+    cout<<sizeof(a)<<endl;
+    cout<<a<<endl;
+}
+
+void fun(int* y)
+{
+    // y = y+1;
+    *y = *y+1;
+    cout<<y<<endl;
+}
+int main()
+{
+
+    //Pointers are passed by value -
+
+    int x =10;
+    int *y = &x;
+    // Dereferencing
+    cout<<&x<<endl;
+    fun(&x);
+    cout<<&x<<endl;
+    cout<<x<<endl;
+    // cout<<y<<" "<<*y;
+    //basic
+    // cout<<&(*y)endl;
+    // cout<<*(&y)<<endl;
+    // // cout<<&(*x)<<endl; invalid statement
+    // cout<<*(&x)<<endl;
+    cout<<"ARRAYS\n";
+    int arr[] = {1,2,3,4};
+    cout<<sizeof(arr)<<endl;
+    cout<<arr<<endl;
+    cout<<*arr<<endl<<*arr+2<<endl<<2[arr]<<endl;
+    array_fun(arr);
+    return 0;
+}
