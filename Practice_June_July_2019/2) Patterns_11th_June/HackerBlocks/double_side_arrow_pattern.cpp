c1a97723362759263f7dcc85127b646394e788d2
ashish nagpal <ashishnagpal2498@gmail.com>
2019-06-18 22:02:01 +0530

    Character array and 2-d array

--- Practice_June_July_2019/2) Patterns_11th_June/HackerBlocks/double_side_arrow_pattern.cpp
+++ Practice_June_July_2019/2) Patterns_11th_June/HackerBlocks/double_side_arrow_pattern.cpp
@@ -29,6 +29,7 @@ int main()
         }
         cout<<endl;
     }
+    // cout<<val<<endl;
     for(int curline=1;curline<=n/2;curline++)
     {
         for(int space=1;space<=2*curline;space++) cout<<" ";
@@ -39,8 +40,9 @@ int main()
         {
             cout<<val; val--;
         }
-        // 
-        for(int middle_space=((n+1)/2) -curline;middle_space>=1;middle_space--) cout<<" ";
+        // Check for middle spacing -
+        //Problem is Here
+        for(int middle_space=((n+1)/2) - 2*(curline-1) -2 ;middle_space>=0;middle_space--) cout<<" ";
         
         for(int nvalue2= 1;nvalue2<=((n+1)/2)-curline&&curline!=(n/2);nvalue2++)
         {
