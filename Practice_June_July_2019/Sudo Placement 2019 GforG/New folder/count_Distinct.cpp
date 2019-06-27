ceb77688aec9d585a436a2bbaef4a17b354bb97f
ashish nagpal <ashishnagpal2498@gmail.com>
2019-06-27 16:55:01 +0530

    Count Distinct Sudo Placement 2019

--- Practice_June_July_2019/Sudo Placement 2019 GforG/New folder/count_Distinct.cpp
+++ Practice_June_July_2019/Sudo Placement 2019 GforG/New folder/count_Distinct.cpp
@@ -1,47 +1,86 @@
 //CountDistinct elements
 #include<iostream>
+#include<set>
 using namespace std;
 void countDistinct(int A[], int k, int n)
 {
     //Your code here
-    int freq_ans[102] = {0};
-    bool initial_freq[102] = {0};
+    //int freq_ans[102] = {0};
+    //bool initial_freq[102] = {0};
     // k tak 
-    // for(int i=0;i<k;i++)
+    //set<int> numbs;
+    //for(int i=0;i<k;i++)
+    //{
+        //initial_freq[arr[i]]++;
+        // freq_ans[arr[i]]++;
+      //  int s = numbs.size();
+       // numbs.insert(A[i]);
+       // if(numbs.size()-s>0)
+       // {
+        //    freq_ans[A[i]]++;
+       // }
+    //}
+    // for(int i=0;i<5;i++)
     // {
-    //     initial_freq[arr[i]]++;
-    //     // freq_ans[arr[i]]++;
+    //     cout<<freq_ans[i]<<" ";
     // }
-    for(int i=0;i<=n-k;i++)
-    {   bool initial_freq[102] = {0};
-        // Window mein check -
-        for(int j=i;j<k+i;j++)
-        {   if(initial_freq[A[j]]==false)
-                {
-                    freq_ans[A[j]]++;
-                    initial_freq[A[j]] = true;
-                }
-        }
-    }
-    for(int i=1;i<=100;i++)
+    // cout<<endl;
+    // for(int i=k;i<n;i++)
+    // {   //bool initial_freq[102] = {0};
+        
+    //     // Window mein check -
+        
+    //     numbs.erase(A[i-k]);
+    //     int s= numbs.size();
+    //     set<int>:: iterator it = numbs.begin();
+    //     for(int i=0;i<s;i++,it++)
+    //     {
+    //         freq_ans[*(it)]++;
+    //     }
+    //     numbs.insert(A[i]);
+    //     if(numbs.size()-s>0){
+    //         freq_ans[A[i]]++;
+
+    //     }
+
+    // }
+    if(n==1&&k==1)
     {
-        if(freq_ans[i]!=0)
-        {
-            cout<<freq_ans[i]<<" ";
-        }
+        cout<<1;
+        return;
     }
+   for(int i=0;i<=n-k;i++)
+        {
+            set<int> s(A+i,A+k+i);                                  //copy the array to set
+            // for(auto i=s.begin();i!=s.end();i++)
+            //     {   cout<<*i<<" ";}
+            //     cout<<endl;
+                cout<<s.size()<<" ";                               //print the size
+ 
+        }
+        cout<<endl;
+   
+    // for(int i=1;i<=100;i++)
+    // {
+    //     if(freq_ans[i]!=0)
+    //     {
+    //         cout<<freq_ans[i]<<" ";
+    //     }
+    // }
     // return 0;
 }
 
 int main()
-{
-    int n;
+{   int T;
+    cin>>T;
+    while(T--)
+    {   int n;
     cin>>n;
     int k;
     cin>>k;
     int arr[105] = {0};
     for(int i=0;i<n;i++) cin>>arr[i];
     countDistinct(arr,k,n);    
-
+    }
     return 0;
 }
