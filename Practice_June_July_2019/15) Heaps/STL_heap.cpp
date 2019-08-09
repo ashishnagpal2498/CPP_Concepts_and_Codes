514a2d7979c502675da528bf4e417265a1dff488
ashish nagpal <ashishnagpal2498@gmail.com>
2019-08-09 15:44:36 +0530

    STL heaps

--- Practice_June_July_2019/15) Heaps/STL_heap.cpp
+++ Practice_June_July_2019/15) Heaps/STL_heap.cpp
@@ -4,12 +4,18 @@
 #include<vector>
 #include<functional>
 using namespace std;
+class myCompare{
+public:
+    bool operator()(int a,int b){
+        return a>b;
+    }
+};
 int main()
 {   
     int arr[] = {1,4,6,2,3,0,10,12};
     int n = sizeof(arr)/sizeof(int);
     priority_queue<int> pmax;
-    priority_queue<int , vector<int> , greater<int> > pq;
+    priority_queue<int , vector<int> , myCompare > pq;
     for(int i=0;i<n;i++)
     {
         pq.push(arr[i]);
