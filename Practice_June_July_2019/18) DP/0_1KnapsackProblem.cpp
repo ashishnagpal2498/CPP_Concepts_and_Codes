4f23385f9d9c794cbe948366f56585e2f45e1111
ashish nagpal <ashishnagpal2498@gmail.com>
2019-09-20 16:42:10 +0530

    Knapsack DP

---
+++ Practice_June_July_2019/18) DP/0_1KnapsackProblem.cpp
@@ -0,0 +1,62 @@
+//0-1 KnapSack Problem - 
+#include<iostream>
+using namespace std;
+//Recursion - max( inclusive , profit - price[n] + f(n-1,w-wt) || exclusive - f(n-1,w))
+int knapSack(int wts[],int prices[],int N,int W)
+{
+    if(N==0||W==0)
+    {   //Either the area is empty or the weight in bag is no more
+        return 0;
+    }
+    int incl = 0,excl =0;
+    if(wts[N-1]<=W)
+    incl = prices[N-1] + knapSack(wts,prices,N-1,W-wts[N-1]);
+    excl = knapSack(wts,prices,N-1,W);
+    return max(incl,excl);
+}
+
+int knapSackDP(int wts[],int prices[],int N,int W)
+{
+    int dp[100][100]={0};
+
+    for(int i=0;i<=N;i++)
+    {
+        for(int w=0;w<=W;w++)
+        {
+            if(w==0||i==0) dp[i][w] = 0;
+            else
+            {
+                int incl=0,excl=0;
+                if(wts[i-1]<=w)
+                {
+                    incl = prices[i-1] + dp[i-1][w-wts[i-1]];
+                }
+                excl = dp[i-1][w];
+                
+                dp[i][w] = max(incl,excl);
+            }
+        }
+    }
+    for(int i=0;i<=N;i++)
+    {
+        for(int j=0;j<=W;j++)
+        {
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return dp[N][W];
+}
+
+int main(int argc, char const *argv[])
+{   int wts[] = {2,2,3,1};
+    int prices[] = {5,20,20,10};
+    int N = 4;
+    int W =5;
+
+    int maxProfit = knapSack(wts,prices,N,W);
+    cout<<"MAX PROFIT   "<<maxProfit<<endl;
+    cout<<"DP KnapSack - "<<knapSackDP(wts,prices,N,W);
+    return 0;
+}
+
--- Practice_June_July_2019/18) DP/minimum_CoinDP.cpp
+++ Practice_June_July_2019/18) DP/minimum_CoinDP.cpp
@@ -1,5 +1,6 @@
 //Minimum Coin Change Problem
 #include<iostream>
+#include<cstring>
 using namespace std;
 int minCoins(int coins[],int amount,int n)
 {
@@ -20,11 +21,34 @@ int minCoins(int coins[],int amount,int n)
     }
     return coinsNeeded;
 }
+int minCoinsDP(int*coins,int amount,int n)
+{
+    int*dp = new int[amount+1];
+    
+    //memset(dp,INT_MAX,(amount+1)*sizeof(int));
+    dp[0]=0;
+    for(int rupay=1;rupay<=amount;rupay++)
+    {   dp[rupay] = INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            if(coins[i]<=rupay)
+            {
+                int smallAns = dp[rupay-coins[i]];
+                if(smallAns!=INT_MAX)
+                {
+                    dp[rupay] = min(dp[rupay],smallAns+1);
+                }
+            }
+        }
+    }
+    return dp[amount];
+}
 int main(int argc, char const *argv[])
 {
     int coins[] = {1,7,10};
     int n = sizeof(coins)/sizeof(int);
     int amount = 15;
     cout<<"MIN COINS "<<minCoins(coins,amount,n);
+    cout<<endl<<"Min "<<minCoinsDP(coins,amount,n);
     return 0;
 }
