e2a098940c3946f8e12dfda5d4622ce7dc5cde33
Ashish Nagpal <ashishnagpal2498@gmail.com>
2020-07-09 17:36:13 +0530

    :up: codeChef Long

--- CodeChef-Problems/New/JulyLongChallenge/chefinaAndSwaps.cpp
+++ CodeChef-Problems/New/JulyLongChallenge/chefinaAndSwaps.cpp
@@ -8,37 +8,103 @@
 using namespace std;
 ll t,n;
 ll arr1[200005],arr2[200005];
-int findIndex(int*A,int l,int h,int* B){
-    int ans = -1,countA=0,countB=0;
-    while(h>l){
-        if(arr[h] == arr[h-1]){
-           ans = h;
-           
-        }
-        h--;
-    }
-    return -1;
-}
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin>>t;
     while(t--){
+        map<ll,ll> A,B; 
         cin>>n;
-        ArrIn(n) cin>>arr1[i];
-        ArrIn(n) cin>>arr2[i];
-        sort(arr1,arr1+n);
-        sort(arr2,arr2+n);
-        int low1=0,hi1=n-1,low2=0,hi2 = n-1;    
-        int cost=0;
-        while(low1<hi1 && low2<hi2){
-            if(arr1[low1] == arr2[low2]){
-                low1++; low2++;
+        ArrIn(n) {
+            cin>>arr1[i];
+            A[arr1[i]]++;
+        }
+        ArrIn(n) {
+            cin>>arr2[i];
+            B[arr2[i]]++;
+        }
+        vector<ll> valuesSwapA,valuesSwapB;
+        map<ll,bool> visited;
+        bool flag = true; 
+        for (auto i: A)
+        {
+            /* code */
+            ll val = i.first;
+            ll valueFreqInA = i.second;
+            ll valueFreqInB = B[val];
+            visited[val] = true;
+            if((valueFreqInA+valueFreqInB)&1){
+                flag = false;
+                break;
             }
-            else {
-                
+            else if(valueFreqInA == valueFreqInB) continue;
+            else if(valueFreqInA>valueFreqInB){
+                int diff = valueFreqInA - valueFreqInB;
+                valuesSwapA.insert(valuesSwapA.begin(),(diff/2),val);
             }
+            else{
+                int diff = valueFreqInB - valueFreqInA;
+                valuesSwapB.insert(valuesSwapB.begin(),(diff/2),val);
+            }
+        }
+        if(!flag){
+            cout<<-1<<endl;
+            continue;
+        }
+        else{
+            for(auto i: B){
+                int val = i.first;
+                if(visited[val]) continue;
+                ll valueFreqInB = i.second;
+            ll valueFreqInA = A[val];
+            visited[val] = true;
+            if((valueFreqInA+valueFreqInB)&1){
+                flag = false;
+                break;
+            }
+            else if(valueFreqInA == valueFreqInB) continue;
+            else if(valueFreqInA>valueFreqInB){
+                int diff = valueFreqInA - valueFreqInB;
+                valuesSwapA.insert(valuesSwapA.begin(),(diff/2),val);
+            }
+            else{
+                int diff = valueFreqInB - valueFreqInA;
+                valuesSwapB.insert(valuesSwapB.begin(),(diff/2),val);
+            }
+            }
+        }
+         if(!flag){
+            cout<<-1<<endl;
+            continue;
+        }
+        else if(valuesSwapA.size()!= valuesSwapB.size()) {cout<<-1<<endl;}
+        else if(valuesSwapA.size() == 0) {cout<<0<<endl;}
+        else
+        {
+        cout<<"Ans -> Compute\n";
+        for(int i=0;i<valuesSwapA.size();i++){cout<<valuesSwapA[i]<<" ";}
+            cout<<endl;
+        for(int i=0;i<valuesSwapB.size();i++){cout<<valuesSwapB[i]<<" ";}
+            cout<<endl;
+        int i=0,j=0;
+        int exchangeVal = 0;
+        ll ans = 0;
+        sort(valuesSwapA.begin(),valuesSwapA.end());
+        sort(valuesSwapB.begin(),valuesSwapB.end());
+        while(exchangeVal<valuesSwapA.size())
+        {   // if array A has smaller element ->
+            if(valuesSwapA[i]<valuesSwapB[j]){
+                ans+=(valuesSwapA[i]);
+                i++;
+            }
+            else{
+                ans+=(valuesSwapB[j]);
+                j++;
+            }
+            exchangeVal++;
+        }
+        cout<<ans<<endl;
         }
     }
     return 0;
