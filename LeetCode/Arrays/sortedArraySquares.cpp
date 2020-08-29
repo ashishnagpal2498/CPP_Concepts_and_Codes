c48a929230e5f6a8536de0c307204888a210066a
Ashish Nagpal <ashishnagpal2498@gmail.com>
2020-08-29 17:54:05 +0530

    :up: Practice

--- LeetCode/Arrays/sortedArraySquares.cpp
+++ LeetCode/Arrays/sortedArraySquares.cpp
@@ -14,11 +14,37 @@ int main()
     cin.tie(NULL);
     cin>>n;
     ArrIn(n) cin>>arr[i];
-    int * ans = new int[n];
-    for(int i=0;i<n;i++){
-        ans[i] = arr[i]*arr[i];
-    }    
-    sort(ans,ans+n);
+    // Since array is sorted we can use two pointer approach
+
+    int* ans = new int[n];
+    int j=0;
+    while(j<n && arr[j]<0) j++;
+    int i = j;
+    j--;
+    int index = 0;
+    while(j>=0 && i<n && index<n){
+        if(arr[j] * arr[j] < arr[i] * arr[i]){
+            ans[index] = arr[j]*arr[j];
+            j--;
+        }
+        else{
+            ans[index] = arr[i]*arr[i];
+            i++;
+        }
+        index++;
+    }
+    while(j>=0){
+        ans[index++] = arr[j]*arr[j--]; 
+    }
+    while(i<n){
+        ans[index++] = arr[i]*arr[i++];
+    }
+
+    // int * ans = new int[n];
+    // for(int i=0;i<n;i++){
+    //     ans[i] = arr[i]*arr[i];
+    // }    
+    // sort(ans,ans+n);
     ArrIn(n){
         cout<<ans[i]<<" ";
     }
