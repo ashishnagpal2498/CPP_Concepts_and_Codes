409fd6a81a1d2edaf27204fe2fda41fe7f49af08
Ashish Nagpal <ashishnagpal2498@gmail.com>
2020-09-03 12:50:29 +0530

    :up: Binary Trees

--- AmazonPrep/binaryTree.cpp
+++ AmazonPrep/binaryTree.cpp
@@ -119,6 +119,8 @@ public:
     }
 };
 myPair2 maxPathSum2(Node* root){
+    // Here -> I can take whichever node -> to start , and ignore its children-> descendants
+    // Therefore -> NodeSum -> 
     myPair2 p;
     if(root==NULL) return p;
 
@@ -153,48 +155,106 @@ class myPair3{
 public:
     int maxV;
     int nodeSum;
-    int leftLeave;
-    int rightLeave;
     myPair3(){
         nodeSum = 0;
-        leftLeave = 0;
-        rightLeave = 0;
         maxV = INT_MIN;
     }
 };
 myPair3 maxLeaveSum(Node* root){
     myPair3 p;
     if(root == NULL) return p;
-
+    if(root->left == NULL && root->right == NULL) {
+        p.nodeSum = root->data;
+        p.maxV = root->data;
+        return p;
+    }
     myPair3 leftTree = maxLeaveSum(root->left);
     myPair3 rightTree = maxLeaveSum(root->right);
 
-    p.leftLeave = max(leftTree.leftLeave,rightTree.leftLeave);
-    p.rightLeave = max(rightTree.rightLeave,leftTree.rightLeave);
+    // Leave comparison ->MAX leave SUM tabhi ho payega when we have both children ->
+    if(root->left && root->right)
+    {  
+        p.maxV = max({leftTree.maxV,rightTree.maxV,leftTree.nodeSum+rightTree.nodeSum+root->data});
+    }
 
-    p.maxV = max({leftTree.maxV,rightTree.maxV,p.leftLeave+p.rightLeave+root->data});
-
+     p.nodeSum = max(leftTree.nodeSum,rightTree.nodeSum)+ root->data;
+    // p.rightLeave = max(rightTree.rightLeave,leftTree.rightLeave);
     return p;
+}
+
+// Maximum Sum Leave to root Path 
+// -> POST ORDER TRAVERSAL ->
+ll leaveToRootSum(Node* root){
+    // Base Case
+    if(root == NULL) return INT_MIN;
+    if(root->left == NULL && root-> right == NULL) return root->data;
+    
+    // cout<<root->data<<" -> ";
+    
+    ll leftTree = leaveToRootSum(root->left);
+    ll rightTree = leaveToRootSum(root->right);
+    
+    return max(leftTree,rightTree)+root->data;
+}
+// PRE_ORDER TRAVERSAL ->
+void leaveToRootSumPreOrder(Node*root,int &maxSum,int currSum,Node*&targetPath){
+    if(root == NULL) return;
+
+    currSum += root->data;
+
+    // path.push_back(root->data);
+    if(root->left == NULL && root->right == NULL){
+        if(currSum > maxSum){
+            maxSum = currSum;
+            targetPath = root;
+        }
+        // else{
+        //     path.pop_back();
+        // }
+        return;
+    }
+    leaveToRootSumPreOrder(root->left,maxSum,currSum,targetPath);
+    leaveToRootSumPreOrder(root->right,maxSum,currSum,targetPath);
+    // path.pop_back();
+}
+//Print Path
+bool printPath(Node* root,Node* targetPath){
+    if(root == NULL) return false;
+
+
+    if(root == targetPath || printPath(root->left,targetPath) || printPath(root->right,targetPath)){
+        cout<<root->data<<" ";
+        return true;
+    }
+    return false;
 }
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
     // cin>>n;
     Node* root = buildTree();
     levelOrderPrint(root);
-    cout<<"Diameter -> "<<diameter(root)<<endl;    
-    myPair ans = diameter2(root);
-    cout<<"Diameter 2 -> "<<ans.diameter<<endl;
-    cout<<"Max value -> path 1 -> "<<maxSum(root)<<endl;
-    myPair2 ans2 = maxPathSum2(root);
-    cout<<"Max value path 2 ->"<<ans2.maxVal<<endl;
-    int ans3 = INT_MIN;
-    vector<int> values;
-    maxXorpath(root,values,ans3);
-    cout<<"Max Xor value is -> "<<ans3<<endl;
+    // cout<<"Diameter -> "<<diameter(root)<<endl;    
+    // myPair ans = diameter2(root);
+    // cout<<"Diameter 2 -> "<<ans.diameter<<endl;
+    // cout<<"Max value -> path 1 -> "<<maxSum(root)<<endl;
+    // myPair2 ans2 = maxPathSum2(root);
+    // cout<<"Max value path 2 ->"<<ans2.maxVal<<endl;
+    // int ans3 = INT_MIN;
+    // vector<int> values;
+    // maxXorpath(root,values,ans3);
+    // cout<<"Max Xor value is -> "<<ans3<<endl;
 
     myPair3 ans4 = maxLeaveSum(root);
     cout<<"Max Leave Sum is -> "<<ans4.maxV<<endl;
+    ll ans5 = leaveToRootSum(root);
+    cout<<"Leave to root max Sum -> "<<ans5<<endl;
+    Node*targetPath = NULL;
+    // vector<int> p;
+    int ans6 = INT_MIN; leaveToRootSumPreOrder(root,ans6,0,targetPath);
+    cout<<"Max Leave to root sum -> "<<ans6<<"\nPath size -> ";
+    printPath(root,targetPath);
+    // for(int i:p) {cout<<i<<" ";}
     return 0;
 }    
