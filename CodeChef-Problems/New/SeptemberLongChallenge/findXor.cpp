8cd11acaedf0dee60e0acca8c60bc8ba08fe1dc5
Ashish Nagpal <ashishnagpal2498@gmail.com>
2020-09-07 17:35:40 +0530

    :up: Codeforces Editorials Practice

---
+++ CodeChef-Problems/New/SeptemberLongChallenge/findXor.cpp
@@ -0,0 +1,23 @@
+// Find XOR - 20 question 10^6 -> 20 bits >
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define ArrIn(n) for(int i=0;i<n;i++)
+#define ArrOut for(int i=0;i<n;i++) {cout<<arr[i]<<" "; }
+#define mod 1e9+7
+using namespace std;
+ll t,n;
+ll arr[200005];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>t;
+    while(t--){
+        cin>>n;
+        // Give input -> it will provide output-->
+        //.
+     
+    }    
+    return 0;
+}    
