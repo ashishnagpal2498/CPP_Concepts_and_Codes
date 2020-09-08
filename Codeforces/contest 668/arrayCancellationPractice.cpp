6622ba27abe501d1bda49ecca90c931b1a5145ff
Ashish Nagpal <ashishnagpal2498@gmail.com>
2020-09-08 23:53:03 +0530

    :up: Codeforces contest 669

--- Codeforces/contest 668/arrayCancellationPractice.cpp
+++ Codeforces/contest 668/arrayCancellationPractice.cpp
@@ -1,22 +1,10 @@
-// POWPOW2
+// Array Cancellation - Editorial
 #include<bits/stdc++.h>
 #define ll long long int
 #define endl "\n"
 #define ArrIn(n) for(int i=0;i<n;i++)
 #define ArrOut for(int i=0;i<n;i++) {cout<<arr[i]<<" "; }
 #define mod 1e9+7
-ll modExp(a,x,m){
-    ll ans=1,a;
-    while(j)
-    {   if(j&1)
-        {
-            ans = (ans * a) % m;
-        }
-        a = (a*a)% m;   
-        j>>=1;
-    }
-    return ans;
-}
 using namespace std;
 ll t,n;
 ll arr[200005];
@@ -24,7 +12,15 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cin>>n;
-        
+    cin>>t;
+    while(t--){
+        cin>>n;
+        ll curr = 0;
+        ArrIn(n){
+            cin>>arr[i];
+            curr = max(0LL,curr+arr[i]);
+        }
+        cout<<curr<<endl;
+    }    
     return 0;
 }    
