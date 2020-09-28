ad26166a5c5a8a7465901388c6982b528c35e74d
Ashish <ashishnagpal2498@gmail.com>
2020-09-28 15:27:38 +0530

    :up: codeforce round 674

--- Codeforces/contest 674 div 3/increaseAndCopy.cpp
+++ Codeforces/contest 674 div 3/increaseAndCopy.cpp
@@ -8,12 +8,22 @@ ll arr[200005];
 int main()
 {           ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
     cin>>t;
     while(t--){
         cin>>n;
-        ll half = n/2;
-        ll moves = half;
-        cout<<moves<<endl;
+        ll copies = n;
+        ll val = 1;
+        ll minVal = INT_MAX;
+        while(val*val<=n+1){
+            ll divide = n/val;
+            if(n%val != 0) divide++;
+            minVal = min({minVal,divide+(val-2)});
+            val++;
+        }
+        if(minVal == INT_MAX) minVal = 0;
+
+        cout<<minVal<<endl;
     }
     return 0;
 }
